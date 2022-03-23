b7942ef1a69c82cb9a5315d1932b758345139344
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-03-23 17:43:38 +0900

    [ADD] #3 - 알림 Activity 생성 및 item String 값 추가

---
+++ app/src/main/java/org/cardna/presentation/ui/mypage/view/setting/AlarmActivity.kt
@@ -0,0 +1,16 @@
+package org.cardna.presentation.ui.mypage.view.setting
+
+import android.os.Bundle
+import com.example.cardna.R
+import com.example.cardna.databinding.ActivityAlarmBinding
+import org.cardna.presentation.base.BaseViewUtil
+
+class AlarmActivity : BaseViewUtil.BaseAppCompatActivity<ActivityAlarmBinding>(R.layout.activity_alarm) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+    }
+
+    override fun initView() {
+    }
+}
