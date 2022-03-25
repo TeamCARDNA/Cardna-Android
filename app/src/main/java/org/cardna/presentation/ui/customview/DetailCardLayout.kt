7e85b6635d32195ecec14f0a5210ec47adc2cafa
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-03-25 16:49:55 +0900

    [ADD] #10 - 카드상세 Custom view 디자인 수정반영 전

---
+++ app/src/main/java/org/cardna/presentation/ui/customview/DetailCardLayout.kt
@@ -0,0 +1,11 @@
+package org.cardna.presentation.ui.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class DetailCardLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr)
