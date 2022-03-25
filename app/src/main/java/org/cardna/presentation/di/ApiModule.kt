81cba69b3a257199a3cc9a943b9392cf9015f25b
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-03-25 13:03:04 +0900

    [ADD] #9 - ApiModule생성

--- app/src/main/java/org/cardna/presentation/di/ApiModule.kt
+++ app/src/main/java/org/cardna/presentation/di/ApiModule.kt
@@ -1,10 +1,62 @@
 package org.cardna.presentation.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.cardna.data.remote.api.*
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ApiModule {
+
+    @Provides
+    @Singleton
+    fun provideAlarmService(retrofit: Retrofit): AlarmService {
+        return retrofit.create(AlarmService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthService(retrofit: Retrofit): AuthService {
+        return retrofit.create(AuthService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCardService(retrofit: Retrofit): CardService {
+        return retrofit.create(CardService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFriendService(retrofit: Retrofit): FriendService {
+        return retrofit.create(FriendService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideInsightService(retrofit: Retrofit): InsightService {
+        return retrofit.create(InsightService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideLikeService(retrofit: Retrofit): LikeService {
+        return retrofit.create(LikeService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMyPageService(retrofit: Retrofit): MyPageService {
+        return retrofit.create(MyPageService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserService(retrofit: Retrofit): UserService {
+        return retrofit.create(UserService::class.java)
+    }
 }
