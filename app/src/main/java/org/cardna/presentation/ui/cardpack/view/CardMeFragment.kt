c9d3b5bf4b4d94790682833e510330c103764c15
minwoo <alsd20177@gmail.com>
2022-03-26 23:58:29 +0900

    [ADD] #12 - 카드나, 카드너 프래그먼트 생성

---
+++ app/src/main/java/org/cardna/presentation/ui/cardpack/view/CardMeFragment.kt
@@ -0,0 +1,12 @@
+package org.cardna.presentation.ui.cardpack.view
+
+import com.example.cardna.R
+import com.example.cardna.databinding.FragmentCardMeBinding
+import org.cardna.presentation.base.BaseViewUtil
+
+class CardMeFragment : BaseViewUtil.BaseFragment<FragmentCardMeBinding>(R.layout.fragment_card_me) {
+
+    override fun initView() {
+    }
+
+}
---
+++ app/src/main/java/org/cardna/presentation/ui/cardpack/view/CardYouFragment.kt
@@ -0,0 +1,12 @@
+package org.cardna.presentation.ui.cardpack.view
+
+import com.example.cardna.R
+import com.example.cardna.databinding.FragmentCardYouBinding
+import org.cardna.presentation.base.BaseViewUtil
+
+
+class CardYouFragment : BaseViewUtil.BaseFragment<FragmentCardYouBinding>(R.layout.fragment_card_you) {
+    override fun initView() {
+    }
+
+}
