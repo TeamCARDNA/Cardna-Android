d017f85f6aa35a08beebe275e991ccb0614ab91f
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-03-27 01:19:24 +0900

    [ADD] #10 - 상세카드 공감수 커스터마이징 전

--- app/src/main/java/org/cardna/presentation/ui/customview/DetailCardLayout.kt
+++ app/src/main/java/org/cardna/presentation/ui/customview/DetailCardLayout.kt
@@ -2,10 +2,64 @@ package org.cardna.presentation.ui.customview
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
+import com.example.cardna.R
+import com.example.cardna.databinding.DetailCardLayoutBinding
 
 class DetailCardLayout @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : ConstraintLayout(context, attrs, defStyleAttr)
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private val binding: DetailCardLayoutBinding = DataBindingUtil.inflate(
+        LayoutInflater.from(context),
+        R.layout.detail_card_layout,
+        this,
+        true
+    )
+
+    @get:JvmName("text")
+    @set:JvmName("text")
+    var text: String
+        get() = getText()
+        set(value) = setText(value)
+
+    init {
+        attrs?.let {
+            context.obtainStyledAttributes(attrs, R.styleable.DetailCardLayout).apply {
+                val title = getString(R.styleable.DetailCardLayout_title)
+                val imageUrl = getString(R.styleable.DetailCardLayout_imageUrl)
+
+                //타이틀이랑 이미지 세팅은 무조건 해줘야함
+                with(binding) {
+                    tvDetailcardTitle.text = title
+                    Glide.with(context).load(imageUrl)
+                        .into(binding.ivDetailcardImage)
+                }
+
+                //카드나/카드너->배경색 바껴야함
+
+                //내가보냐
+
+                //남이보냐
+
+                //보관함이냐
+            }
+        }
+    }
+
+    private fun initLayout(){
+
+
+
+    }
+
+    private fun getText() = binding.tvDetailcardTitle.text.toString()
+    private fun setText(text: String) {
+        binding.tvDetailcardTitle.setText(text)
+    }
+}
