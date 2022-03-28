e8055a27bfe0edfc29b8dd87606a321de30b5166
minwoo <alsd20177@gmail.com>
2022-03-28 10:50:01 +0900

    [ADD] #12 - 액티비티, 바텀씻 프래그먼트 추가

---
+++ app/src/main/java/org/cardna/presentation/ui/cardpack/view/BottomDialogCardFragment.kt
@@ -0,0 +1,51 @@
+package org.cardna.ui.cardpack
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.cardna.databinding.FragmentBottomDialogCardBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class BottomDialogCardFragment(val itemClick: (Int) -> Unit) : BottomSheetDialogFragment() {
+    private var _binding: FragmentBottomDialogCardBinding? = null
+    private val binding get() = _binding ?: error("Binding이 초기화되지 않았습니다")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBottomDialogCardBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        makeCard()
+    }
+
+    private fun makeCard(){
+        binding.clBottomdialogCardTop.setOnClickListener{
+            itemClick(CARD_ME)
+            // 컴패니언 상수인 cardme = 1이라면 cardme를 activity에 정의된 함수의 인자값으로 넘겨서 그거에 따라서 실행
+            dialog?.dismiss()
+            // 일단 다이얼로그는 무조건 없어지긴 해야함
+        }
+
+        binding.clBottomdialogCardBottom.setOnClickListener{
+            itemClick(CARD_YOU)
+            dialog?.dismiss()
+        }
+    }
+
+    companion object{
+        const val CARD_ME = 0
+        const val CARD_YOU = 1
+    }
+}
---
+++ app/src/main/java/org/cardna/presentation/ui/cardpack/view/FriendCardPackActivity.kt
@@ -0,0 +1,12 @@
+package org.cardna.presentation.ui.cardpack.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cardna.R
+
+class FriendCardPackActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friend_card_pack)
+    }
+}
