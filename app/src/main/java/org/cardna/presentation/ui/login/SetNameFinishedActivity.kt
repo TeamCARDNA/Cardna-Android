da621a16307eae02804dfbdc7c4261d955f2d5c5
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-04-03 00:17:47 +0900

    [FEAT] #18 - 카드추가 유도뷰 애니메이션 구현

--- app/src/main/java/org/cardna/presentation/ui/login/SetNameFinishedActivity.kt
+++ app/src/main/java/org/cardna/presentation/ui/login/SetNameFinishedActivity.kt
@@ -1,14 +1,21 @@
 package org.cardna.presentation.ui.login
 
+import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Spannable
+import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.core.text.set
 import androidx.core.text.toSpannable
 import com.example.cardna.R
 import com.example.cardna.databinding.ActivitySetNameFinishedBinding
 import dagger.hilt.android.AndroidEntryPoint
+import org.cardna.CardNaApplication
 import org.cardna.presentation.base.BaseViewUtil
 import org.cardna.presentation.util.LinearGradientSpan
+import org.cardna.presentation.util.StatusBarUtil
 
 @AndroidEntryPoint
 class SetNameFinishedActivity :
@@ -19,9 +26,47 @@ class SetNameFinishedActivity :
     }
 
     override fun initView() {
+        StatusBarUtil.setStatusBar(this, Color.BLACK)
+        getScreenHeight()
         setSleep()
         setClickListener()
         setTextView()
+    }
+
+    private fun getScreenHeight() {
+        val screenHeight = CardNaApplication.pixelRatio.screenHeight
+        if (screenHeight > 2872 || screenHeight < 2560) {
+            setUpAnim(R.anim.anim_translate_up_2560height, 2300L)
+        } else if (screenHeight in 2560..2872) {
+            setUpAnim(R.anim.anim_translate_up_2872height, 4300L)
+        }
+    }
+
+    private fun setUpAnim(animId: Int, delayMillis: Long) {
+        val animation = AnimationUtils.loadAnimation(this, animId)
+        binding.ctlSetnamefinished.startAnimation(animation)
+        Handler(Looper.getMainLooper()).postDelayed({ setFadeAnim() }, delayMillis)
+    }
+
+    private fun setFadeAnim() {
+        with(binding.ctlSetnamefinishedMessage) {
+            visibility = View.VISIBLE
+            val fadeIn = AnimationUtils.loadAnimation(this@SetNameFinishedActivity, R.anim.fade_in)
+            val fadeOut = AnimationUtils.loadAnimation(this@SetNameFinishedActivity, R.anim.fade_out)
+            startAnimation(fadeOut)
+            startAnimation(fadeIn)
+            Handler(Looper.getMainLooper()).postDelayed({ setButtonAnim() }, 670)
+        }
+    }
+
+    private fun setButtonAnim() {
+        with(binding.llSetnamefinishedBtn) {
+            visibility = View.VISIBLE
+            val fadeIn = AnimationUtils.loadAnimation(this@SetNameFinishedActivity, R.anim.fade_in)
+            val fadeOut = AnimationUtils.loadAnimation(this@SetNameFinishedActivity, R.anim.fade_out)
+            startAnimation(fadeOut)
+            startAnimation(fadeIn)
+        }
     }
 
     private fun setClickListener() {
@@ -30,7 +75,7 @@ class SetNameFinishedActivity :
     }
 
     private fun setTextView() {
-        val welcomeText = intent.getStringExtra("welcomeText")
+        val welcomeText = intent.getStringExtra("welcomeText") ?: "반가워요"
 
         with(binding) {
             tvSetnamefinishedTitle.text = setGradientText(welcomeText!!)
