b82d377e504da7f5db2c8308e6da4835a6152263
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-04-02 18:24:07 +0900

    [FEAT] #18 - 팝업 커스텀 전

--- app/src/main/java/org/cardna/presentation/ui/detailcard/view/DetailCardActivity.kt
+++ app/src/main/java/org/cardna/presentation/ui/detailcard/view/DetailCardActivity.kt
@@ -2,24 +2,28 @@ package org.cardna.presentation.ui.detailcard.view
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import android.view.View
-import android.view.WindowInsetsController
-import android.view.WindowManager
-import android.widget.Button
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.*
+import android.widget.*
 import androidx.activity.viewModels
 import androidx.annotation.IdRes
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import com.example.cardna.R
 import com.example.cardna.databinding.ActivityDetailCardBinding
 import dagger.hilt.android.AndroidEntryPoint
+import org.cardna.presentation.MainActivity
 import org.cardna.presentation.base.BaseViewUtil
 import org.cardna.presentation.ui.detailcard.viewmodel.DetailCardViewModel
 import org.cardna.presentation.util.*
+import kotlin.math.roundToInt
 
 @AndroidEntryPoint
 class DetailCardActivity : BaseViewUtil.BaseAppCompatActivity<ActivityDetailCardBinding>(R.layout.activity_detail_card) {
@@ -47,8 +51,8 @@ class DetailCardActivity : BaseViewUtil.BaseAppCompatActivity<ActivityDetailCard
     @SuppressLint("ResourceType")
     private fun setObserve() {
         detailCardViewModel.detailCard.observe(this) { detailCard ->
-      //      cardType = detailCard.type
-     cardType = "you"
+            //      cardType = detailCard.type
+            cardType = "you"
             setSrcWithGlide(detailCard.cardImg, binding.ivDetailcardImage)
 
             with(binding) {
@@ -61,7 +65,8 @@ class DetailCardActivity : BaseViewUtil.BaseAppCompatActivity<ActivityDetailCard
                     }
                     CARD_YOU -> {
                         tvDetailcardTitle.setBackgroundResource(R.drawable.bg_mainpurple_stroke_real_black_2dp)
-                        showEditDialog(R.layout.dialog_detail_cardyou)
+                        //   showEditDialog(R.layout.dialog_detail_cardyou)
+                        show()
                     }
                     STORAGE -> {
                         tvDetailcardTitle.setBackgroundResource(R.drawable.bg_white_1_5_stroke_real_black_2dp)
@@ -70,6 +75,92 @@ class DetailCardActivity : BaseViewUtil.BaseAppCompatActivity<ActivityDetailCard
                 }
             }
         }
+    }
+
+
+    fun show() {
+        val items = this.resources.getStringArray(R.array.detail_cardyou)
+        val popupAdapter =
+            object : ArrayAdapter<String>(baseContext, R.layout.item_detail, items) {
+                override fun getView(
+                    position: Int,
+                    convertView: View?,
+                    parent: ViewGroup
+                ): View {
+                    val view = super.getView(position, convertView, parent)
+                    val color = if (position == 0) {
+                        R.color.black
+                    } else {
+                        R.color.black
+                    }
+                    (view as TextView).setTextColor(ContextCompat.getColor(context, color))
+                    return view
+                }
+            }
+
+        val popup = ListPopupWindow(this).apply {
+            anchorView = binding.ibtnDetailcardEdit
+            setAdapter(popupAdapter)
+            setDropDownGravity(Gravity.NO_GRAVITY)
+         //   width = dpToPx(112)
+           width = measureContentWidth(baseContext,popupAdapter)
+            height = ListPopupWindow.WRAP_CONTENT
+            isModal=true
+       //  horizontalOffset = -300
+            verticalOffset = -20
+           Log.d("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ",promptPosition.toString())
+
+            setBackgroundDrawable(ContextCompat.getDrawable(baseContext, R.drawable.img_popup))
+        }
+
+        popup.setOnItemClickListener { _, view, _, _ ->
+            /*      if ((view as TextView).text == "수정하기") {
+                      commentUpdateListener(data)
+                      popup.dismiss()
+                  } else {
+                      commentDeleteListener(data)
+                      popup.dismiss()
+                  }
+              }*/
+        }
+        popup.show()
+    }
+
+
+    private fun measureContentWidth(context: Context, adapter: ListAdapter): Int {
+        val measureParentViewGroup = FrameLayout(context)
+        var itemView: View? = null
+
+        var maxWidth = 0
+        var itemType = 0
+
+        val widthMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+        val heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+
+        for (index in 0 until adapter.count) {
+            val positionType = adapter.getItemViewType(index)
+            if (positionType != itemType) {
+                itemType = positionType
+                itemView = null
+            }
+            itemView = adapter.getView(index, itemView, measureParentViewGroup)
+            itemView.measure(widthMeasureSpec, heightMeasureSpec)
+            val itemWidth = itemView.measuredWidth
+            if (itemWidth > maxWidth) {
+                maxWidth = itemWidth
+            }
+        }
+        return maxWidth
+    }
+
+    fun convertDPtoPX(dp: Int): Int {
+        val density: Float = this.resources.displayMetrics.density
+        return (dp.toFloat() * density).roundToInt()
+    }
+
+    fun dpToPx(dp: Int): Int {
+        val displayMetrics = resources.displayMetrics
+        return (dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT)).roundToInt()
     }
 
     @SuppressLint("ResourceType")
