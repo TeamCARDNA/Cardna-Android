ec5b573f40bff891cadd58dbc585aeb96164408b
username <ieejo716@naver.com>
2022-05-07 17:13:31 +0900

    [FIX] : 이전 내용으로 수정

--- app/src/main/java/org/cardna/CardNaApplication.kt
+++ app/src/main/java/org/cardna/CardNaApplication.kt
@@ -13,7 +13,7 @@ class CardNaApplication : Application() {
         super.onCreate()
         initPixelUtil()
         initLogger()
-        initKakaoLogin()
+//        initKakaoLogin()
         initFirebaseApp()
     }
 
@@ -25,10 +25,10 @@ class CardNaApplication : Application() {
         Timber.plant(Timber.DebugTree())
     }
 
-    private fun initKakaoLogin() {
-        val kakaoAppKey = BuildConfig.KAKAO_APP_KEY
-        KakaoSdk.init(this, kakaoAppKey)
-    }
+//    private fun initKakaoLogin() {
+//        val kakaoAppKey = BuildConfig.KAKAO_APP_KEY
+//        KakaoSdk.init(this, kakaoAppKey)
+//    }
 
     private fun initFirebaseApp() {
         FirebaseApp.initializeApp(this)
