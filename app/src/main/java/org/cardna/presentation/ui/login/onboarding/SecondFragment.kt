6a3a80e60d57e74e16f4cc230f3d649991b0de56
username <ieejo716@naver.com>
2022-05-17 01:16:52 +0900

    [FEAT] #6 - 온보딩 뷰 수정

--- app/src/main/java/org/cardna/presentation/ui/login/onboarding/SecondFragment.kt
+++ app/src/main/java/org/cardna/presentation/ui/login/onboarding/SecondFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import org.cardna.R
 import org.cardna.databinding.FragmentSecondBinding
 import org.cardna.presentation.base.BaseViewUtil
+import org.cardna.presentation.util.setSpannableColor
 
 class SecondFragment : BaseViewUtil.BaseFragment<FragmentSecondBinding>(R.layout.fragment_second) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -16,6 +17,8 @@ class SecondFragment : BaseViewUtil.BaseFragment<FragmentSecondBinding>(R.layout
     }
 
     override fun initView() {
-
+        val inputText = binding.tvSecondAbout.text.toString()
+        binding.tvSecondAbout.text =
+            requireActivity().setSpannableColor(inputText, R.color.main_purple, 5, 22)
     }
 }
