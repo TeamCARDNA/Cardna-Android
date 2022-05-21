16a73423736b5e6ff91db0f8bcde8398a416488c
username <ieejo716@naver.com>
2022-05-21 22:35:24 +0900

    [FIX] :  #82 - maincard 제약 수정, MainCardActivity -> FriendCardPackActivity

--- app/src/main/java/org/cardna/presentation/ui/login/view/LoginActivity.kt
+++ app/src/main/java/org/cardna/presentation/ui/login/view/LoginActivity.kt
@@ -151,38 +151,6 @@ class LoginActivity :
         }
     }
 
-    private fun getErrorLog(error: Throwable) {
-        when {
-            error.toString() == AuthErrorCause.AccessDenied.toString() -> {
-                Timber.e("접근이 거부 됨(동의 취소)")
-            }
-            error.toString() == AuthErrorCause.InvalidClient.toString() -> {
-                Timber.e("유효하지 않은 앱")
-            }
-            error.toString() == AuthErrorCause.InvalidGrant.toString() -> {
-                Timber.e("인증 수단이 유효하지 않아 인증할 수 없는 상태")
-            }
-            error.toString() == AuthErrorCause.InvalidRequest.toString() -> {
-                Timber.e("요청 파라미터 오류")
-            }
-            error.toString() == AuthErrorCause.InvalidScope.toString() -> {
-                Timber.e("유효하지 않은 scope ID")
-            }
-            error.toString() == AuthErrorCause.Misconfigured.toString() -> {
-                Timber.e("설정이 올바르지 않음(android key hash)")
-            }
-            error.toString() == AuthErrorCause.ServerError.toString() -> {
-                Timber.e("서버 내부 에러")
-            }
-            error.toString() == AuthErrorCause.Unauthorized.toString() -> {
-                Timber.e("앱이 요청 권한이 없음")
-            }
-            else -> { // Unknown
-                Timber.e("기타 에러")
-            }
-        }
-    }
-
     private fun startMainActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
